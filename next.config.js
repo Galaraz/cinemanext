6c26e4646f9d3a6e484ec844f0268b8078713fe7
galaraz <jvictor.si.2013@gmail.com>
2023-05-02 11:35:12 -0400

    app dir teste

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  appDir: true,
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     runtime: 'edge',
-    appDir: true,
+    
   }
 }
 
