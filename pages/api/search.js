ebc9f85e810dd897ed0250dd95b980660da2222f
galaraz <jvictor.si.2013@gmail.com>
2023-05-04 16:50:21 -0400

    get server

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,6 +1,8 @@
 import { apiBase, apikey } from "../../lib/tmdb";  
 
-
+export const config = {
+   runtime: 'experimental-edge',
+ }
 export default async (req, res) => {
     
    const result = await fetch(`${apiBase}/search/movie?api_key=${apikey}&language=pt-BR&query=${req.query.q}`);
