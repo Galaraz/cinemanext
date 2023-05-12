1c969e65770f74f0a9ce304f8b0edda333981a6a
galaraz <jvictor.si.2013@gmail.com>
2023-05-12 09:02:42 -0400

    update

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,10 +1,9 @@
 import { apiBase, apikey } from "../../lib/tmdb";  
 
-export const config = {
-   runtime: 'experimental-edge',
- }
 export default async (req, res) => {
-    
+   if (req.query.q === undefined || req.query.q === null){
+      req.query.q = 'brasil'
+   }else{req.query.q = 'brasil'}
    const result = await fetch(`${apiBase}/search/movie?api_key=${apikey}&language=pt-BR&query=${req.query.q}`);
    const json = await result.json();
    const sucess =res.status(200).json({list: json.results}); 
